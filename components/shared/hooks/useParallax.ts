9826b02c8d5e7d7d60082e433c283f158f094152
ukijo <nes-you-ukijo@hotmail.fr>
2023-05-20 15:07:14 +0100

    refactor: remove logs from useParallax hook

--- components/shared/hooks/useParallax.ts
+++ components/shared/hooks/useParallax.ts
@@ -1,23 +1,16 @@
-import { useTransform, useScroll } from 'framer-motion';
-import { useEffect, RefObject } from 'react';
+import { useTransform, useScroll } from "framer-motion";
+import { RefObject } from "react";
 
 export const useParallax = (ref: RefObject<HTMLDivElement>) => {
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ['start start', 'end start'],
+    offset: ["start start", "end start"],
   });
   const y = useTransform(
     scrollYProgress,
     [0, 1],
     [0, -400 * (Math.random() * 4)]
   );
-
-  useEffect(() => {
-    return scrollYProgress.onChange((latest) => {
-      console.log('Page scroll: ', latest);
-      console.log('y: ', y.get());
-    });
-  }, []);
 
   return y;
 };
